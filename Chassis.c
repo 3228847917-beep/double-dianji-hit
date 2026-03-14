131a7eddf8a3383fc3a5121ec19258c3954fb826
CLLR <3228847917@qq.com>
2026-03-14 18:32:39 +0800

    Vx与Vy出现交换实现宏观正常

--- Chassis.c
+++ Chassis.c
@@ -4,14 +4,17 @@
 #include "Task_Init.h"
 #include "semphr.h"
 
+
+//PID依次为5，0.0001，30
+
 //电机驱动
 Motor_param motor1 = {
 .PID = {
-	.Kp = 0.0f,
+	.Kp = 1.0f,
 	.Ki = 0.0f,
 	.Kd = 0.0f,
 	.limit = 10000.0f,
-	.output_limit = 60.0f,
+	.output_limit = 40.0f,
 },
 .steering={
 	.motor_id=0x01,
@@ -20,11 +23,11 @@ Motor_param motor1 = {
 };
 Motor_param motor2 = {
 .PID = {
-	.Kp = 0.0f,
+	.Kp = 1.0f,
 	.Ki = 0.0f,
 	.Kd = 0.0f,
 	.limit = 10000.0f,
-	.output_limit = 60.0f,
+	.output_limit = 40.0f,
 },
 .steering={
 	.motor_id=0x02,
@@ -33,14 +36,14 @@ Motor_param motor2 = {
 };
 Motor_param motor3 = {
 .PID = {
-	.Kp = 0.0f,
+	.Kp = 1.0f,
 	.Ki = 0.0f,
 	.Kd = 0.0f,
 	.limit = 10000.0f,
-	.output_limit = 60.0f,
+	.output_limit = 40.0f,
 },
 .steering={
-	.motor_id=0x04,
+	.motor_id=0x03,
 	.hcan = &hcan2,
 }
 };
@@ -66,9 +69,9 @@ volatile float v1 = 0.0f;
 volatile float v2 = 0.0f;
 volatile float v3 = 0.0f;
 
-volatile float wheel_one = 0.0f;  //前左
-volatile float wheel_two = 0.0f;  //前右
-volatile float wheel_three=0.0f;  //后右
+volatile float wheel_one = 0.0f;
+volatile float wheel_two = 0.0f;
+volatile float wheel_three=0.0f;
 
 TaskHandle_t Remote_Handle;
 void Remote(void *pvParameters)
@@ -79,18 +82,19 @@ void Remote(void *pvParameters)
 	{
 		if(MODE == REMOTE)
 		{			
-			v1 = Vx -LENGTH * Wz;
-			v2 = Vx*0.5f+ Vy*(sqrt(3.0f)/2.0) + LENGTH * Wz;
-			v3 = Vx*0.5f-Vy*(sqrt(3.0f)/2.0) + LENGTH * Wz;
+			v1 = -Vx*0.5f+Vy*(sqrt(3.0f)/2.0) + R * Wz;
+			v2 = -Vx*0.5f- Vy*(sqrt(3.0f)/2.0) + R * Wz;
+			v3 = Vx + R * Wz;
 			
-			wheel_one=  (int16_t)((v1 / (2.0f * PI * WHEEL_RADIUS)) * 60.0f);
-			wheel_two=  (int16_t)((v2 / (2.0f * PI * WHEEL_RADIUS)) * 60.0f);
-			wheel_three=(int16_t)((v3 / (2.0f * PI * WHEEL_RADIUS)) * 60.0f);
+			wheel_one=  -((v1 / (2.0f * PI * WHEEL_RADIUS)) * 60.0f);
+			wheel_two=  ((v2 / (2.0f * PI * WHEEL_RADIUS)) * 60.0f);
+			wheel_three=-((v3 / (2.0f * PI * WHEEL_RADIUS)) * 60.0f);
 			
 			PID_Control2((float)(motor1.steering.epm / 7.0f/(3.4f)), wheel_one, &motor1.PID);
 			PID_Control2((float)(motor2.steering.epm / 7.0f/(3.4f)), wheel_two, &motor2.PID);
 			PID_Control2((float)(motor3.steering.epm / 7.0f/(3.4f)), wheel_three,&motor3.PID);
 
+			vTaskDelay(1);
       VESC_SetCurrent(&motor1.steering, motor1.PID.pid_out);
       VESC_SetCurrent(&motor2.steering, motor2.PID.pid_out);
 	    VESC_SetCurrent(&motor3.steering, motor3.PID.pid_out);  
@@ -100,13 +104,13 @@ void Remote(void *pvParameters)
 		{
 			wheel_one = 0;
 			wheel_two = 0;
-			wheel_three = 0;
+			wheel_three=0;
 			
 			VESC_SetCurrent(&motor1.steering, 0);
 			VESC_SetCurrent(&motor2.steering, 0);
 			VESC_SetCurrent(&motor3.steering, 0);
 		}
-			vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(2));
+		vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(2));
 	}
 }
 extern SemaphoreHandle_t remote_semaphore;
@@ -126,7 +130,9 @@ void Move_Remote(void *pvParameters){
             Remote_Control.Eomega = RemoteData.rocker[2];
             Remote_Control.mode = RemoteData.rocker[3];
             Remote_Control.Key_Control = &RemoteData.Key;
-        }else{
+        }
+				else
+				{
             Remote_Control.Ex = 0;
             Remote_Control.Ey = 0;
             Remote_Control.Eomega = 0;
@@ -139,23 +145,23 @@ void Move_Remote(void *pvParameters){
      if(MODE == REMOTE)
       {
 					//遥控映射
-            Vx = -(Remote_Control.Ex / 2047.0f) * MAX_VELOCITY;
-            Vy = -(Remote_Control.Ey / 2047.0f) * MAX_VELOCITY;
+            Vy = -(Remote_Control.Ex / 2047.0f) * MAX_VELOCITY;
+            Vx = -(Remote_Control.Ey / 2047.0f) * MAX_VELOCITY;
             Wz = (Remote_Control.Eomega / 2047.0f) * MAX_OMEGA;
-			//进行击球动作（flag转到hitball.c）
-				if(Remote_Control.First.Right_Key_Up== 1 && Remote_Control.Second.Right_Key_Up == 0)
-		  	{
-		  		flag = 1;
-			  }
+//			//进行击球动作（flag转到hitball.c）
+//			if(Remote_Control.First.Right_Key_Up== 1 && Remote_Control.Second.Right_Key_Up == 0)
+//		  	{
+//		  		flag = 1;
+//			  }
       }
-				vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(1));
+		vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(2));
     }
 }
 
 void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
 	uint8_t Recv[8] = {0};
-	uint32_t ID = CAN_Receive_DataFrame(hcan, Recv);
+	uint32_t ID = CAN_Receive_DataFrame(&hcan2, Recv);
 	VESC_ReceiveHandler(&motor1.steering, &hcan2, ID,Recv);
 	VESC_ReceiveHandler(&motor2.steering, &hcan2, ID,Recv);
 	VESC_ReceiveHandler(&motor3.steering, &hcan2, ID,Recv);
