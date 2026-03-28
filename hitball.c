585d48ef2d66b452b7b41ad7b49ec04afbdff656
CLLR <3228847917@qq.com>
2026-03-28 09:39:39 +0800

    新增为4个光电门

--- hitball.c
+++ hitball.c
@@ -4,11 +4,15 @@
 #include "RobStride.h"
 #include "step.h"
 
-//GPIO_PinState GPIOA8_State = GPIO_PIN_SET;
-//GPIO_PinState GPIOC9_State = GPIO_PIN_SET;
-//GPIO_PinState GPIOC2_State = GPIO_PIN_SET;
+//用于气缸电磁阀的GPIO:
+GPIO_PinState GPIOA8_State = GPIO_PIN_SET;
+GPIO_PinState GPIOC9_State = GPIO_PIN_SET;
+
+//备用
 //GPIO_PinState GPIOC3_State = GPIO_PIN_SET;
 //GPIO_PinState GPIOB10_State = GPIO_PIN_SET;
+
+//作为光电门捕获的GPIO: 
 //GPIO_PinState GPIOB11_State = GPIO_PIN_SET;
 //GPIO_PinState GPIOB12_State = GPIO_PIN_SET;
 //GPIO_PinState GPIOB13_State = GPIO_PIN_SET;
@@ -21,35 +25,36 @@ uint8_t flag = 0;
 TaskHandle_t Volleyball_Serve_Handle; 
 void Volleyball_Serve(void *pvParameters)
 {
-	TickType_t last_wake_time = xTaskGetTickCount();
+	TickType_t last_wake_time = xTaskGetTickCount();	  
   for(;;)
 	  {		
-		//光电门读取电平
+		//读取电平 
 		key1 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_11);
 		key2 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12);
 		key3 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13);
 		if(key1 == GPIO_PIN_SET || key2 == GPIO_PIN_SET || key3 == GPIO_PIN_SET)
 			{
-			flag = 1;	
+			flag = 1;	//这里做自动发球，手动发球方式暂存（chassis.c）
 			if(flag == 1)
 			  {
 				hit_ball_trigger = 1;
-	          }
+	       }
+	    }
+		 if(hit_ball_trigger == 1)
+			{
+//此为测试使用
+			  	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIOA8_State);
+			  	HAL_GPIO_WritePin(GPIOC,GPIO_PIN_9,GPIOC9_State);
+				//
+//			    //启动电磁阀门进行击球				
+//					HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_SET);
+//					HAL_GPIO_WritePin(GPIOC,GPIO_PIN_9,GPIO_PIN_SET);
+//					vTaskDelay(500);
+//					HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_RESET);
+//					HAL_GPIO_WritePin(GPIOC,GPIO_PIN_9,GPIO_PIN_RESET);
+//					hit_ball_trigger = 0;
+//					flag = 2;
 			}
-				if(hit_ball_trigger == 1)
-				{
-			    //启动电磁阀门进行击球
-					HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_SET);
-					HAL_GPIO_WritePin(GPIOC,GPIO_PIN_9,GPIO_PIN_SET);
-					HAL_GPIO_WritePin(GPIOC,GPIO_PIN_2,GPIO_PIN_SET);
-					vTaskDelay(1500);
-					HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_RESET);
-					HAL_GPIO_WritePin(GPIOC,GPIO_PIN_9,GPIO_PIN_RESET);
-					HAL_GPIO_WritePin(GPIOC,GPIO_PIN_2,GPIO_PIN_RESET);
-					hit_ball_trigger = 0;
-					flag = 2;
-				}
 			vTaskDelayUntil(&last_wake_time, pdMS_TO_TICKS(2));
 		 }
 }
-
